dfa588952b77daae182eba61b2a6d802354206c8
Michael Telatynski <7t3chguy@gmail.com>
2015-05-23 14:20:43 +0100

    fix

--- classes/StatsToHTML.js
+++ classes/StatsToHTML.js
@@ -116,7 +116,8 @@ var StatsToHTML = function(statsAnalyzer, channelConfig, startTime, version) {
 					// Write the rendered HTML in the index.html file there
 					fs.writeFile( this.channelConfig.destination + '/index.html', html, 'utf8', function() {
 
-						fs.writeFile( this.channelConfig.destination + '/data.json', this.JSONData, 'utf8' function() {
+						fs.writeFile( this.channelConfig.destination + '/data.json', this.JSONData, 'utf8', function() {
+							Logger.log('MESSAGE', '## ## ## Written DATA JSON!');
 							this.copyTemplateFiles.bind(this)(onReadyCallback);
 						}).bind(this) );
 						// Once that's done, copy the rest of the template files
